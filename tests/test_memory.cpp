91d3780e5514dbc5329eefc52515b8f178d5eb43
neoJINXD <anik_patel@live.com>
2020-12-23 17:06:32 -0500

    Fixed broken test and setup for memory leak test

---
+++ tests/test_memory.cpp
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+
+int main()
+{
+    // Initialize the library
+    if (!glfwInit())
+        return -1;
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(1024, 768, "Project Eagle", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current 
+    glfwMakeContextCurrent(window);
+
+    // WARNING Make sure to initialize GLFW window and set as main context otherwise this fails
+    // Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to create GLEW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+    // Shader* sh = new Shader("../shaders/vert.glsl","../shaders/frag.glsl");
+
+    printf("ptr to int\n");
+    int* t = new int(0);
+
+    
+    printf("ptr should be dead by now\n");
+    delete (t);
+
+    glfwTerminate();
+
+    // delete (sh);
+
+    return 0;
+}
