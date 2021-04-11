e3d9234db094f930e5e412a891f87e3c20262175
neoJINXD <neojinxd@gmail.com>
2021-04-11 15:08:07 -0400

    added callbacks to glfw window

--- Eagle/src/Platform/OpenGL/OpenGLWindow.cpp
+++ Eagle/src/Platform/OpenGL/OpenGLWindow.cpp
@@ -3,6 +3,8 @@
 #include "OpenGLWindow.h"
 
 #include "Core/Events/ApplicationEvent.h"
+#include "Core/Events/KeyboardEvent.h"
+#include "Core/Events/MouseEvent.h"
 
 static bool GLFWInitialized = false;
 
@@ -38,9 +40,92 @@ Eagle::OpenGLWindow::OpenGLWindow(const std::string& _title, unsigned int _width
 		data.callback(e);
 	});
 
-	/*glfwSetKeyCallback(window, [](GLFWwindow* win, int keycode, int scancode, int action, int mods) {
+	glfwSetWindowFocusCallback(window, [](GLFWwindow* window, int focusStatus) {
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
-	});*/
+		switch (focusStatus)
+		{
+		case GLFW_TRUE:
+		{
+			WindowFocusEvent e;
+			data.callback(e);
+			break;
+		}
+		case GLFW_FALSE:
+		{
+			WindowUnfocusEvent e;
+			data.callback(e);
+			break;
+		}
+		}
+	});
+
+	glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+		WindowResizeEvent e(width, height);
+		data.callback(e);
+	});
+
+	glfwSetKeyCallback(window, [](GLFWwindow* window, int keycode, int scancode, int action, int mods) {
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+		switch (action)
+		{
+		case GLFW_PRESS:
+		{
+			KeyDownEvent e(keycode);
+			data.callback(e);
+			break;
+		}
+		case GLFW_RELEASE:
+		{
+			KeyUpEvent e(keycode);
+			data.callback(e);
+			break;
+		}
+		case GLFW_REPEAT:
+		{
+			// TODO still need to setup key holding
+			KeyDownEvent e(keycode);
+			data.callback(e);
+			break;
+		}
+		}
+	});
+	
+	glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int buttonID, int action, int mods) {
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+		
+		switch (action)
+		{
+		case GLFW_PRESS:
+		{
+			MouseButtonDownEvent e(buttonID);
+			data.callback(e);
+			break;
+		}
+		case GLFW_RELEASE:
+		{
+			MouseButtonUpEvent e(buttonID);
+			data.callback(e);
+			break;
+		}
+		}
+	});
+
+	glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xPos, double yPos) {
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+		
+		MouseMovedEvent e((float)xPos, (float)yPos);
+		data.callback(e);
+	});
+
+	glfwSetScrollCallback(window, [](GLFWwindow* window, double xDelta, double yDelta) {
+		WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+		MouseScrollEvent e((float)xDelta, (float)yDelta);
+		data.callback(e);
+	});
 }
 
 Eagle::OpenGLWindow::~OpenGLWindow()
