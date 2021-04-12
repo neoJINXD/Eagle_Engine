39d9b2ccc0fdbe0dcc5b293670351e61829806f6
neoJINXD <neojinxd@gmail.com>
2021-04-12 00:22:13 -0400

    added more options for logging

--- Eagle/src/Core/Log.h
+++ Eagle/src/Core/Log.h
@@ -29,13 +29,15 @@ EAGLE_DISABLE_WARNING_POP
 }
 
 
-#define ENGINE_LOG(...)  Eagle::Log::getEngineLog()->info(__VA_ARGS__)
-#define ENGINE_WARN(...) Eagle::Log::getEngineLog()->warn(__VA_ARGS__)
-#define ENGINE_ERR(...)  Eagle::Log::getEngineLog()->error(__VA_ARGS__)
+#define ENGINE_TRACE(...)     Eagle::Log::getEngineLog()->trace(__VA_ARGS__)
+#define ENGINE_LOG(...)       Eagle::Log::getEngineLog()->info(__VA_ARGS__)
+#define ENGINE_WARN(...)      Eagle::Log::getEngineLog()->warn(__VA_ARGS__)
+#define ENGINE_ERR(...)	      Eagle::Log::getEngineLog()->error(__VA_ARGS__)
+#define ENGINE_CRITICAL(...)  Eagle::Log::getEngineLog()->critical(__VA_ARGS__)
 
 
 #define DEBUG_LOG(...)  Eagle::Log::getApplicationLog()->info(__VA_ARGS__)
-#define DEBUG_ERR(...)  Eagle::Log::getApplicationLog()->error(__VA_ARGS__)
 #define DEBUG_WARN(...) Eagle::Log::getApplicationLog()->warn(__VA_ARGS__)
+#define DEBUG_ERR(...)  Eagle::Log::getApplicationLog()->error(__VA_ARGS__)
 
 #define PRINT(...) DEBUG_LOG(__VA_ARGS__)
